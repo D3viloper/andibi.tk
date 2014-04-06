f0cfce7bb9474642d36bd168a9c8973f2b005f98
Vincent <centzilius@w1r3.net>
2014-04-06 13:51:34 +0200

    added things and moved things around

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Configfile
+
+// Pageinformation
+$config_page = array("maintitle" => "ANDIBI.TK");
+$config_navbar_content = array(
+                                array("text" => "Home", "page_idf" => "index"),
+                                array("text" => "Über", "page_idf" => "about"),
+                                array("text" => "Kontakt", "page_idf" => "about"),
+                                array("text" => "Internet Special", "page_idf" => "internet"),
+                                array("dropdown" => "true", "text" => "Tools", "page_idf_dropdown" => "dropdown", "elements" => array(array("text" => "Chrome <3 HTML5", "page_idf" => "chromenewhtml5"),
+                                                                                                                                      array("text" => "Gefährliches Copy & Paste", "page_idf" => "cp",
+                                                                                                                                      array("text" => "User-Agent", "page_idf" => "ua")
+                            );
+?>
