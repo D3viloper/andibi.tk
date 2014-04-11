af29d2311f8feacbcd0795c887ac18cacfda7bd4
Brawl345 <brawl98@outlook.com>
2014-04-11 19:27:16 +0200

    more content

--- pages/internet.php
+++ pages/internet.php
@@ -16,6 +16,9 @@ include("templates/navbar.php");
                     <iframe src="http://free.timeanddate.com/countdown/i432vs5y/n1014/cf12/cm0/cu5/ct0/cs0/ca0/co1/cr0/ss0/cac428bca/cpcf00/pct/tcfff/fs175/szw576/szh243/tatSchaltung%20des%20Internets%20in/tac000/tptInternet%20ist%20geschalten%20seit/tpc000/iso2014-04-16T12:00:00" frameborder="0" width="749" height="150"></iframe>
                 </div><br><br>
                 <h1>Updates immer am Wochenende!</h1>
+                <h2>07. April 2014</h2>
+                <p>Das Tablet ist angekommen!</p>
+                <br>
                 <h2>05. April 2014</h2>
                 <p>1&1 hat die Pakete versandt!</p>
                 <br>
