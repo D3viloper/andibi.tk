5dbca18fdff78c9fdc97a1c86a5929c983e5ec38
Brawl345 <brawl98@outlook.com>
2014-04-12 20:32:19 +0200

    moved open source global message to the footer and used the new way of deactivating global messages!

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
     <div id="footer">
       <div class="container">
-        <p class="text-muted credit">&copy; 2013-2014 iCON
+        <p class="text-muted credit">&copy; 2013-2014 iCON - <a href="https://github.com/Brawl345/andibi.tk" target="_blank">Code auf GitHub</a>
         <?php echo "$zusatz"; ?></p>
       </div>
     </div>
