16338ba16df33ef5969c788fd6992a43c980d6bb
Vincent <centzilius@w1r3.net>
2014-04-14 16:03:57 +0200

    fixed 404 page

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ if ($_GET['page']) {
 	if(file_exists("pages/" . $_GET['page'] . ".php")) {
 		$file_path = "pages/" . $_GET['page'] . ".php";
 		include($file_path);
+	} else {
+		header("Status: 404 Not Found"); // Gebe 404 aus
+		include("pages/404.php"); // gebe die 404 Seite aus, für den Fall, dass keine ErrorPage gesetzt wurde
 	}
 } else {
 	include("pages/index.php");
