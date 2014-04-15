5ef3768924a876c50c6b85b198556d8aa0726ca7
Brawl345 <brawl98@outlook.com>
2014-04-16 00:37:21 +0200

    more content for the GREATEST INTERNET START IN THE HISTORY!

--- pages/internet.php
+++ pages/internet.php
@@ -10,14 +10,18 @@ include("templates/navbar.php");
                 <h1>
                     ANDIBI goes online!</h1>
                 <div class="error-details">
-                  <h1>Es ist so weit! Endlich wieder reguläres Internet nach einem Jahr! Am <b>16.04.2014</b>*!</h1>
+                  <h1>Es ist so weit! Endlich wieder reguläres Internet nach einem Jahr! <b>HEUTE</b>*!</h1>
                 </div>
                 <div class="error-actions">
                     <iframe src="https://freesecure.timeanddate.com/countdown/i432vs5y/n1014/cf12/cm0/cu5/ct0/cs0/ca0/co1/cr0/ss0/cac428bca/cpcf00/pct/tcfff/fs175/szw576/szh243/tatSchaltung%20des%20Internets%20in/tac000/tptInternet%20ist%20geschalten%20seit/tpc000/iso2014-04-16T12:00:00" frameborder="0" width="749" height="150"></iframe>
+                    <p>Gegen 17:00 Uhr</p>
                 </div><br><br>
                 <h1>Updates immer am Wochenende!</h1>
+                <h2>16. April 2014</h2>
+                <p>Der Termin für den 16.04.2014 hat sich nicht geändert!</p>
+                <br>
                 <h2>07. April 2014</h2>
-                <p>Das Tablet ist angekommen!</p>
+                <p>Das Tablet (und natürlich der ganze Rest) ist angekommen!</p>
                 <br>
                 <h2>05. April 2014</h2>
                 <p>1&1 hat die Pakete versandt!</p>
