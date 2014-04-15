f6150314fa2164e7dabeb74857d8e49e99f92ea1
Brawl345 <brawl98@outlook.com>
2014-04-15 13:08:16 +0200

    load countdown over HTTPS (I use HTTPS for my local webserver)

--- pages/internet.php
+++ pages/internet.php
@@ -13,7 +13,7 @@ include("templates/navbar.php");
                   <h1>Es ist so weit! Endlich wieder reguläres Internet nach einem Jahr! Am <b>16.04.2014</b>*!</h1>
                 </div>
                 <div class="error-actions">
-                    <iframe src="http://free.timeanddate.com/countdown/i432vs5y/n1014/cf12/cm0/cu5/ct0/cs0/ca0/co1/cr0/ss0/cac428bca/cpcf00/pct/tcfff/fs175/szw576/szh243/tatSchaltung%20des%20Internets%20in/tac000/tptInternet%20ist%20geschalten%20seit/tpc000/iso2014-04-16T12:00:00" frameborder="0" width="749" height="150"></iframe>
+                    <iframe src="https://freesecure.timeanddate.com/countdown/i432vs5y/n1014/cf12/cm0/cu5/ct0/cs0/ca0/co1/cr0/ss0/cac428bca/cpcf00/pct/tcfff/fs175/szw576/szh243/tatSchaltung%20des%20Internets%20in/tac000/tptInternet%20ist%20geschalten%20seit/tpc000/iso2014-04-16T12:00:00" frameborder="0" width="749" height="150"></iframe>
                 </div><br><br>
                 <h1>Updates immer am Wochenende!</h1>
                 <h2>07. April 2014</h2>
