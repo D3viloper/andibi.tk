a305143a533ba500ca78b2286172b18d6f03f72c
Brawl345 <brawl98@outlook.com>
2014-04-30 01:58:57 +0200

    remove --enable-easy-off-store-extension-install on the chrome-extensions page, because it doesn't exist anymore

--- pages/chrome-extension.php
+++ pages/chrome-extension.php
@@ -26,9 +26,8 @@ include("templates/navbar.php");
           <p>Öffne dann die "Erweiterungen"-Seite in Google Chrome. Öffne dazu das <img class="img-responsive" src="img/allgoogle/assets/chrome_menu.png" alt="3 horizontale Streifen"> Chrome-Menü und           klicke dann auf "Tools" -> "Erweiterungen".</p>
           <img class="img-responsive" src="img/allgoogle/assets/erweiterungen.png" alt="Tools und dann auf Erweiterungen">
           <p>Ziehe dann die <?php echo $extension ?>-Erweiterung von deinem Dateien-Explorer in das Erweiterungs-Fenster und lege die Erweiterung ab.</p>
-          <p><strong>HINWEIS:</strong> Falls die Installation so nicht funktioniert, lade dir entweder die
-          <a href="https://www.google.com/chrome/eula.html?extra=devchannel" target="_blank">Entwicklerversion von Google Chrome herunter</a> oder füge
-          <i>"<a href="http://peter.sh/experiments/chromium-command-line-switches/#easy-off-store-extension-install" target="_blank">--enable-easy-off-store-extension-install            </a>"</i> zu Chromes "Eigenschaften" hinzu (unter "Ziel").</p>
+          <p><strong>HINWEIS:</strong> Falls die Installation so nicht funktioniert, lade dir die
+          <a href="https://www.google.com/chrome/eula.html?extra=devchannel" target="_blank">Entwicklerversion von Google Chrome herunter</a>.</p>
           <img class="img-responsive" src="img/allgoogle/assets/erweiterung_installieren.png" alt="Erweiterung in das Fenster ziehen">
           <p>Bestätige noch den Hinweis und schon ist die <?php echo $extension ?>-Erweiterung installiert!</p>
 
