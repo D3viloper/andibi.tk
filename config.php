7e399917bb9b18da1b821a2a5dfc1020f7055b30
Brawl345 <brawl98@outlook.com>
2014-05-12 23:34:00 +0200

    added an info page for the arch linux repository

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ $config_navbar_content = array(
                                 array("text" => "Home", "page_idf" => "index"),
                                 array("text" => "Über", "page_idf" => "about"),
                                 array("text" => "Kontakt", "page_idf" => "contact"),
+                                array("text" => "Arch Linux Repo", "page_idf" => "archrepo"),
                                 array("dropdown" => "true", "text" => "Tools", "dropdown_idf" => "tools", "elements" => array(array("text" => "Chrome &lt;3 HTML5", "page_idf" => "chromenewhtml5"),
                                                                                                                                       array("text" => "Gefährliches Copy & Paste", "page_idf" => "cp"),
                                                                                                                                       array("text" => "User-Agent", "page_idf" => "ua")))
---
+++ pages/archrepo.php
@@ -0,0 +1,41 @@
+<?php
+$shownpage_page = array("page_idf" => "archrepo", "title" => "Arch Linux Repo", "description" => "[andi] Arch Repo");
+include("templates/headInclude.php");
+include("templates/navbar.php");
+?>
+    <div class="container">
+	    <?php include("templates/meldungen.php"); ?>
+	  <div class="page-header">
+		<h1>ANDIBI.TK Arch Linux Repo</h1>
+	  </div>
+        <div class="pull-right"><img src="uburemix/archlogo.png" height="187" width="187"></div>
+        <p>ANDIBI.TK hat eine eigene Arch-Repo!</p>
+        <p>Ich habe diese erstellt, da ich das Kompilieren aus dem AUR hasse (dauert ewig) und ich anderen das ersparen möchte. Hier findest du eine Auswahl an interessanten Paketen. Auch ist einige Software in der [andi] Repo aktueller. Die Repo ist momentan noch im Aufbau.</p>
+        
+    <div class="page-header">
+        <h1>Füge die Repo hinzu</h1>
+    </div>
+        <div class="alert alert-warning">
+          <p>Die Repo unterstützt momentan nur x86_64 (64 Bit)!</p>
+        </div>
+          <p>Öffne die <code>/etc/pacman.conf</code> in einem Editor mit Root-Rechten und füge folgendes ganz unten hinzu:</p>
+          <div class="well">
+            <p>[andi]<br>
+            SigLevel = Optional TrustAll<br>
+            Server = http://andibi.tk/archrepo
+          </div>
+          <p>Lade dann mit <code>sudo pacman -Sy</code> die Repos neu!</p>
+        
+    <div class="page-header">
+        <h1>Welche Pakete sind enthalten?</h1>
+    </div>
+        <p>Hier ist eine Liste:</p>
+        <?php include("archrepo/packagelist.php"); ?>
+
+    </div><!-- /.container -->
+</div>
+	<!-- Footer -->
+<?php
+include("templates/footer.php");
+include("templates/htmlEnd.php");
+?>
