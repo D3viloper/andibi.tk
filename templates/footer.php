88ed33a60c6c3a436393097f2390d52c22e706b0
Brawl345 <brawl98@outlook.com>
2014-05-16 20:10:54 +0200

    show last commit in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,9 @@
     <div id="footer">
       <div class="container">
         <p class="text-muted credit">&copy; 2013-2014 iCON - <a href="https://github.com/Brawl345/andibi.tk" target="_blank">Code auf GitHub</a>
-        <?php echo "$zusatz"; ?></p>
+        <?php echo "$zusatz";
+        exec('git rev-parse --verify HEAD 2> /dev/null', $output);
+	$hash = $output[0];
+	echo ' - <a href="https://github.com/Brawl345/andibi.tk/commit/'.$hash.'" target="_blank">'.$hash.'</a>'; ?></p>
       </div>
     </div>
