497a620a7a35022670c437f8428aabf2e5c20842
Brawl345 <brawl98@outlook.com>
2014-05-16 19:00:01 +0200

    revert commits 2a25edf2f7f2657356d3da69bef606ef968e8e2c and 6288fdd85f1eba339793e74fb4afd9c8ba4f1c87, because they break local editing

--- templates/headInclude.php
+++ templates/headInclude.php
@@ -7,23 +7,23 @@
   	<meta name="description" content="<?php echo $shownpage_page['description']; ?>">
   	<meta name="author" content="Andreas Bielawski">
   	<meta http-equiv="expires" content="86400">
-  	<link rel="shortcut icon" href="/favicon.ico">
+  	<link rel="shortcut icon" href="favicon.ico">
   	<title><?php echo $config_page['maintitle'] . " | " . $shownpage_page['title']; ?></title>
   	<!-- Bootstrap core CSS -->
-  	<link rel="stylesheet" href="/static/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="static/css/bootstrap.min.css">
   	<!-- Custom styles for this page -->
-  	<link rel="stylesheet" href="/static/css/template.css">
+  	<link rel="stylesheet" href="static/css/template.css">
   	<?php if($shownpage_page['page_idf'] == "404" || $shownpage_page['page_idf'] == "internet"):?>
   	<!-- Custom styles for this error page -->
-  	<link rel="stylesheet" href="/static/css/404.css">
+  	<link rel="stylesheet" href="static/css/404.css">
     <?php endif;?>
     <?php if($shownpage_page['genericons'] == "true"): ?>
-    <link href="/static/css/genericons.css" rel="stylesheet">
-	<link href="/static/css/custom-genericons.css" rel="stylesheet">
+    <link href="static/css/genericons.css" rel="stylesheet">
+	<link href="static/css/custom-genericons.css" rel="stylesheet">
     <?php endif;?>
     <?php if($shownpage_page['page_idf'] == "discover"): ?>
     <!-- Custom styles for this preview -->
-    <link href="/static/css/carousel.css" rel="stylesheet">
+    <link href="static/css/carousel.css" rel="stylesheet">
     <?php endif; ?>
   	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
--- templates/htmlEnd.php
+++ templates/htmlEnd.php
@@ -2,6 +2,6 @@
     </div><!-- /.container -->
     <?php endif; ?>   
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="/static/js/bootstrap.min.js"></script>
+    <script src="static/js/bootstrap.min.js"></script>
 </body>
 </html>
