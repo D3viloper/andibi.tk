35f39837d1a9b8e0e533cf8c8d78baa9ff32ae0b
Brawl345 <brawl98@outlook.com>
2014-05-16 20:24:37 +0200

    revert last commit, because it (sadly) doesn't work on the server...

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,6 @@
     <div id="footer">
       <div class="container">
         <p class="text-muted credit">&copy; 2013-2014 iCON - <a href="https://github.com/Brawl345/andibi.tk" target="_blank">Code auf GitHub</a>
-        <?php echo "$zusatz";
-        exec('git rev-parse --verify HEAD 2> /dev/null', $output);
-	$hash = $output[0];
-	echo ' - <a href="https://github.com/Brawl345/andibi.tk/commit/'.$hash.'" target="_blank">'.$hash.'</a>'; ?></p>
+        <?php echo "$zusatz"; ?></p>
       </div>
     </div>
