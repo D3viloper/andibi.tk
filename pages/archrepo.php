16163dd0ba95889621393fdbde62973da7187974
Brawl345 <brawl98@outlook.com>
2014-06-28 22:27:11 +0200

    updated archrepo page to reflect the changes to the signed repo

--- pages/archrepo.php
+++ pages/archrepo.php
@@ -10,21 +10,33 @@ include("templates/navbar.php");
 	  </div>
         <div class="pull-right"><img src="uburemix/archlogo.png" height="187" width="187" alt="ArchLinux Logo"></div>
         <p>ANDIBI.TK hat eine eigene Arch-Repo!</p>
-        <p>Ich habe diese erstellt, da ich das Kompilieren aus dem AUR hasse (dauert ewig) und ich anderen das ersparen möchte. Hier findest du eine Auswahl an interessanten Paketen. Auch ist einige Software in der [andi] Repo aktueller. Die Repo ist momentan noch im Aufbau.</p>
+        <p>Ich habe diese erstellt, da ich das Kompilieren aus dem AUR hasse (dauert ewig) und ich anderen das ersparen möchte. Hier findest du eine Auswahl an interessanten Paketen. Auch ist einige Software in der [andi] Repo aktueller.</p>
         
     <div class="page-header">
         <h1>Füge die Repo hinzu</h1>
     </div>
         <div class="alert alert-warning">
-          <p>Die Repo unterstützt momentan nur x86_64 (64 Bit)!</p>
+          <p>Die Repo unterstützt nur x86_64 (64 Bit)!</p>
         </div>
           <p>Öffne die <code>/etc/pacman.conf</code> in einem Editor mit Root-Rechten und füge folgendes ganz unten hinzu:</p>
           <div class="well">
             <p>[andi]<br>
-            SigLevel = Optional TrustAll<br>
             Server = http://andibi.tk/archrepo
           </div>
           <p>Lade dann mit <code>sudo pacman -Sy</code> die Repos neu!</p>
+          <p><strong>Die Repo und alle Pakete sind von mir seit dem 28. Juni 2014 signiert.</strong> Pacman wird dich fragen, ob du den Key importieren möchtest. Wähle "J".</p>
+          
+    <div class="page-header">
+        <h1>Problembehebung</h1>
+    </div>
+    <p><i><strong>Ich habe die Repo vor dem 28. Juni 2014 genutzt. Was muss ich jetzt tun?</strong></i><br>
+    Du musst in deiner <code>/etc/pacman.conf</code> die Zeile mit "SigLevel" unter [andi] entfernen. Sollte es beim Neuladen der Repositorys Probleme geben, führe <code>sudo pacman -Syy</code> aus.</p>
+    
+    <p><i><strong>Der Key kann nicht importiert werden??</strong></i><br>
+    Es kann sein, dass dein ISP den Keyserver blockt. Ändere in der <code>/etc/pacman.d/gnupg/gpg.conf</code> den Keyserver zu <code>hkp://pgp.mit.edu:11371</code> ab und versuche es erneut.</p>
+    
+    <p><i><strong>Es klappt immer noch nicht!</strong></i><br>
+    Führe folgenden Befehl aus: <code>sudo pacman-key -r A4FA956F && sudo pacman-key --lsign A4FA956F</code> - dies importiert meinen Key manuell und signiert ihn lokal.</p>
         
     <div class="page-header">
         <h1>Welche Pakete sind enthalten?</h1>
