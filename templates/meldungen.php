482f2f03bcd628e37034d7ef3a7bed718b8d5f30
Brawl345 <brawl98@outlook.com>
2014-07-07 18:04:26 +0200

    last update

--- templates/meldungen.php
+++ templates/meldungen.php
@@ -5,5 +5,5 @@ if($config_global_meldung['activated'] == "true"):
 else:
     echo '<div class="text-center"><p>Momentan ist keine Meldung verfügbar</p></div>';
 endif;
-echo modal2($content='<div class="text-center"><strong>Dies ist eine Preview-Version, live aus dem <a href="https://github.com/Brawl345/andibi.tk/tree/gstyle" target="_blank">Git-Repositorium</a>!</strong> Bitte <a href="https://github.com/Brawl345/andibi.tk/issues" target="_blank">melde auftretende Fehler</a>!</div>');
+echo modal2($content='');
  ?>
