dfdc8dbdfb77c92f26e05eb3f02c676be4457d91
Brawl345 <brawl98@outlook.com>
2014-07-27 14:06:39 +0200

    - removed IE edge, because IE takes Edge as default (zu deutsch: IE braucht keine Extrawurst)

--- templates/headInclude.php
+++ templates/headInclude.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- NO <meta http-equiv="X-UA-Compatible" content="IE=edge">, because fuck you IE -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo $shownpage_page['description']; ?>">
     <meta name="author" content="Andreas Bielawski">
