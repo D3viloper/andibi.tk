328e7cd98eca6318af12dc11344f9732a2d9e532
Andreas Bielawski <brawl98@outlook.com>
2015-01-01 13:28:12 +0100

    update copyright to 2015

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
 </div> <!-- /wrap -->
 <div id="footer">
       <div class="container col-md-12 text-center">
-        <p class="text-muted credit">&copy; 2013-2014 iCON - <a href="https://github.com/Brawl345/andibi.tk/tree/master/pages/<?php echo $shownpage_page['page_idf']; ?>.php" target="_blank">Code dieser Seite auf GitHub</a>
+        <p class="text-muted credit">&copy; 2013-2015 iCON - <a href="https://github.com/Brawl345/andibi.tk/tree/master/pages/<?php echo $shownpage_page['page_idf']; ?>.php" target="_blank">Code dieser Seite auf GitHub</a>
         <?php echo "$zusatz"; ?></p>
       </div>
     </div>
