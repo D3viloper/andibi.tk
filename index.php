fe58895fb0eebb81e007f79ec48cc9a3df4c651f
Andreas Bielawski <brawl98@outlook.com>
2016-02-08 17:10:07 +0100

    moar

--- index.php
+++ index.php
@@ -11,6 +11,27 @@ if(file_exists("config.php")) {
         echo "Die config.php existiert nicht";
         exit;
     }
+	
+function sanitize_output($buffer) {
+
+    $search = array(
+        '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',  // strip whitespaces before tags, except space
+        '/(\s)+/s'       // shorten multiple whitespace sequences
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1'
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}
+
+ob_start("sanitize_output");
 
 include("lib/meldung.php");
 include("lib/modal.php");
