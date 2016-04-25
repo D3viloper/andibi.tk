4ad36129174439b53c71eb3e78f74054b606f0f5
Andreas Bielawski <brawl98@outlook.com>
2016-04-25 22:02:35 +0200

    remove filterlist site from nav

--- config.php
+++ config.php
@@ -6,7 +6,6 @@ $config_page = array("maintitle" => "ANDIBI.TK", "site_url" => "https://andibi.t
 $config_navbar_content = array(
                                 array("text" => "Home", "page_idf" => "index"),
                                 array("text" => "Über", "page_idf" => "about"),
-                                array("text" => "Brawls Blocking-List", "page_idf" => "abp"),
                                 array("dropdown" => "true", "text" => "Tools", "dropdown_idf" => "tools", "elements" => array(
 																															  array("text" => "Datalist-Test", "page_idf" => "datalisttest"),
                                                                                                                               array("text" => "Gefährliches Copy &amp; Paste", "page_idf" => "cp"),
@@ -14,5 +13,5 @@ $config_navbar_content = array(
 																															  )
 								     )
                             );
-$config_global_meldung = array("activated" => "false", "bell_count" => "1", "level" => "success", "message" => '<strong>Willkommen bei ANDIBI.TK!</strong><br> Das neue Design, welches das  <strong><a href="http://www.google.com/design/" target="_blank">Material-Design</a></strong> von Google benutzt, ist endlich online. Bitte <strong><a href="'.$root.'contact.php">melde auftretende Fehler</a></strong>. Den Code kannst du <strong><a href="https://github.com/Brawl345/andibi.tk/" target="_blank">auf GitHub betrachten</a></strong>.');
+$config_global_meldung = array("activated" => "false", "bell_count" => "1", "level" => "success", "message" => '');
 ?>
--- pages/abp.php
+++ pages/abp.php
@@ -1,5 +1,6 @@
 <?php
-  $shownpage_page = array("page_idf" => "abp", "title" => "Brawls Blocking List", "description" => "ANDIBI.TK - Brawls Adblock Plus Blocking List");
+  $shownpage_page = array("page_idf" => "abp", "title" => "Brawls Blocking List (eingestellt)", "description" => "ANDIBI.TK - Brawls Adblock Plus Blocking List");
+  $page_has_extra_navbarentry = "true";
   include("templates/headInclude.php");
   include("templates/navbar.php");
 ?>
@@ -9,8 +10,8 @@
     <div class="col-md-8">
     <div class="jumbotron">
         <h1>Brawls Blocking List</h1>
-        <p>Filterliste für Adblock Plus - filtert nervige, dumme Sachen im Web!</p>
-          <a class="btn btn-lg btn-success" href="abp:subscribe?location=https%3A%2F%2Fandibi.tk%2Ffilterlist.txt&amp;title=Brawls%20Blocking%20List" role="button">Filterliste abonnieren</a>
+        <p>Filterliste für Adblock Plus - filtert nervige, dumme Sachen im Web (Updates eingestellt).</p>
+          <a class="btn btn-lg btn-success" href="abp:subscribe?location=https%3A%2F%2Fandibi.tk%2Ffilterlist.txt&amp;title=Brawls%20Blocking%20List" role="button">Filterliste abonnieren (Updates eingestellt)</a>
         <a class="btn btn-lg btn-warning" href="https://adblockplus.org/de/" role="button" target="_blank">Adblock Plus holen</a>
     </div>
     </div>
@@ -21,6 +22,7 @@
       <h4>Was ist das?</h4>
     </div>
     <div class="panel-body">
+	<p><strong>HINWEIS:</strong> Seit dem 25. Januar 2016 gab es keine Updates mehr, der Support und die Updates wurden am 25. April 2016 endgültig eingestellt. Trotzdem danke für dein Interesse!</p>
     <p>Das ist eine Filterliste für <a href="https://adblockplus.org/de/" target="_blank">Adblock Plus</a> oder andere Adblock-Erweiterungen, wie <a href="https://www.ublock.org/" target="_blank">µBlock</a>.<br>
 	Sie filtert einige nervige Dinge im Web, bspw. Freiräume, übersehen Banner, zappelende und andere, nervige Dinge.</p>
 	<p>Die Filterliste aktualisiert sich selbstständig alle drei Tage und wird mit <a href="https://github.com/r4vi/block-the-eu-cookie-shit-list/tree/master/genlist" target="_blank">Genlist</a> erstellt.</p>
@@ -48,17 +50,14 @@
     <div class="col-md-6">
     <div class="panel panel-default">
     <div class="panel-heading">
-      <h4>Anregungen, Vorschläge, Kritik?</h4>
+      <h4>Updates eingestellt</h4>
     </div>
     <div class="panel-body">
-    <p>Vorschläge für die Filterliste, oder irgendwas wird zu viel geblockt? Du kannst mich auf Telegram oder per Mail kontaktieren.</p>
-    <div class="clearfix"></div>
-      <hr>
-    <a href="https://telegram.me/Brawl" target="_blank" class="list-group-item active shadow-z-2">Per Telegram kontaktieren</a><br>
-	<a href="http://wiidatabase.de/kontakt" target="_blank" class="list-group-item active shadow-z-2">Per Mail kontaktieren</a>
+    <p>Danke für dein Interesse. Die Weiterentwicklung der Filterliste wurde eingestellt. Ich nehme keine Vorschläge, etc. mehr an.</p>
     </div>
     </div>
     </div>
+	
     </div><!--/row-->
     <!-- Footer -->
 <?php include("templates/footer.php");
